f60aff24539018cd549aee6651b4fa0c02fb9577
Eivind Gard Lund <e.g.lund@medisin.uio.no>
2016-11-10 15:13:47 +0100

    tweaked sbatch file. has issues with SNP alignment and reference for phasing

--- phase.bash
+++ phase.bash
@@ -1,16 +1,15 @@
 #!/bin/bash
 #SBATCH --time=00:05:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=6000
-#SBATCH --output=logs/align_%j.txt
+#SBATCH --mem=600
+#SBATCH --output=logs/phase_%j.txt
 
+set -u
+set -e
 #prefix="${bed%.bed}"
 chr=$(basename "$prefix")
 echo "Phasing $prefix ..."
 srun shapeit --input-bed "$prefix" \
 	--input-map genetic_map_b37/genetic_map_${chr}_combined_b37.txt \
-	--thread 4
 	--output-max "${prefix}.phased.haps" "${prefix}.phased.sample" \
 	--output-log "${prefix}.phased.log"
 srun shapeit -convert \
